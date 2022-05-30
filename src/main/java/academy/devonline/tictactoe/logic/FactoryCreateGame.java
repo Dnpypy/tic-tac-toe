9cb19e522249d1276663cf5a58544ae9d1facdc8
Dnpypy <denlatypoff@yandex.ru>
2022-05-30 15:47:58 +0300

    Implements the GameFactory logic

--- src/main/java/academy/devonline/tictactoe/logic/FactoryCreateGame.java
+++ src/main/java/academy/devonline/tictactoe/logic/FactoryCreateGame.java
@@ -22,15 +22,17 @@ import academy.devonline.tictactoe.model_data.TypeStepPlayer;
 
 import static academy.devonline.tictactoe.model_data.Transfers.O;
 import static academy.devonline.tictactoe.model_data.Transfers.X;
+import static academy.devonline.tictactoe.model_data.TypeStepPlayer.COMPUTER;
+import static academy.devonline.tictactoe.model_data.TypeStepPlayer.USER;
 
 /**
  * @author Dnpypy
  */
 public class FactoryCreateGame {
 
-    private TypeStepPlayer player1;
+    private final TypeStepPlayer playerType1;
 
-    private TypeStepPlayer player2;
+    private final TypeStepPlayer playerType2;
 
 
     /**
@@ -40,13 +42,31 @@ public class FactoryCreateGame {
      * @param args аргументы командной строки
      */
     public FactoryCreateGame(final String[] args) {
-//        for (String arg : args) {
-//            if(arg.equals( TypeStepPlayer.USER)){
-//                player1 = TypeStepPlayer.USER;
-//            } else if(arg.equals(TypeStepPlayer.COMPUTER)){
-//                player2 = TypeStepPlayer.COMPUTER;
-//            }
-//        }
+        TypeStepPlayer playerType1 = null;
+        TypeStepPlayer playerType2 = null;
+        for (String arg : args) {
+            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
+                if (playerType1 == null) {
+                    playerType1 = TypeStepPlayer.valueOf(arg.toUpperCase());
+                } else if (playerType2 == null) {
+                    playerType2 = TypeStepPlayer.valueOf(arg.toUpperCase());
+                } else {
+                    System.err.println("Unsupported command line argument: '" + arg + "'");
+                }
+            } else {
+                System.err.println("Unsupported command line argument: '" + arg + "'");
+            }
+        }
+        if (playerType1 == null) {
+            this.playerType1 = USER;
+            this.playerType2 = COMPUTER;
+        } else if (playerType2 == null) {
+            this.playerType1 = USER;
+            this.playerType2 = playerType1;
+        } else {
+            this.playerType1 = playerType1;
+            this.playerType2 = playerType2;
+        }
     }
 
     /**
@@ -54,15 +74,28 @@ public class FactoryCreateGame {
      */
     public TicTacToeGame gameCreate() {
         final ConverterCell converterCell = new NumKeyCellNumbConvertDesktop();
+        final PlayerXO player1;
+        if (playerType1 == USER) {
+            player1 = new PlayerXO(X, new PlayerMove(converterCell));
+        } else {
+            player1 = new PlayerXO(X, new MoveAl());
+        }
 
+        final PlayerXO player2;
+        if (playerType2 == USER) {
+            player2 = new PlayerXO(O, new PlayerMove(converterCell));
+        } else {
+            player2 = new PlayerXO(O, new MoveAl());
+        }
+        final boolean moveSecondPlayer = playerType1 != playerType2;
         return new TicTacToeGame(
                 new BoardPrint(converterCell),
-//                player1 != null ?  new PlayerXO(X, new PlayerMove(converterCell)) : new PlayerXO(O, new MoveAl());
-                new PlayerXO(X, new PlayerMove(converterCell)),
-                new PlayerXO(O, new MoveAl()),
+                player1,
+                player2,
                 new CheckWinner(),
                 new CellDraw(),
-                true);
+                moveSecondPlayer
+        );
 
     }
 }
